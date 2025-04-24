8ad0686ce280f59c90892403cfba32b6baeaf709
lily-diamond <brielle.laplante22@gmail.com>
2025-04-24 09:29:42 -0700

    Lab 5 completed

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - print out a car
+// Author: Brielle LaPlante
+// Date: 4/24/2025
 
 // Constants
 
@@ -14,8 +14,26 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
+
+  //start
+  let make = "Ford";
+  let model = "Taurus";
+  let carYear = 1995;
+  let color = "Blue";
+  let currentYear = 2025;
+  let carAge = currentYear - carYear;
+  
+  
+  document.writeln("Make: "+make+"<br>");
+  document.writeln("Model: "+model+"<br>");
+  document.writeln("Color: "+color+"<br>");
+  document.writeln("Year: "+carYear+"<br>");
+  //document.writeln("Current Year: "+currentYear+"<br>");
+  document.writeln("Age: "+carAge+" years<br>");
+
   // the code that makes everything happen
 }
 
 // let's get this party started
 main();
+
