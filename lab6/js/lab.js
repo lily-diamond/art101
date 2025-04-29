b347dc1a959e228b0e105c4e29871f2a1fdac27f
lily-diamond <brielle.laplante22@gmail.com>
2025-04-28 20:58:39 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - print transport
+// Author: Brielle LaPlante
+// Date: 4/28/25
 
 // Constants
 
@@ -15,6 +15,22 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
+  let myMainRide = { 
+    make: "Chevrolet",
+    model:"Volt",
+    color:"Gray",
+    year: 2014,age: function() {
+      return 2025-this.year;
+    }
+  };
+
+  let myTransport = ["Chevy Volt", " Bus"];
+  document.writeln("Getting Around: "+myTransport+"<br>");
+  document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, "\t","</pre>"));
+
+
+
 }
 
 // let's get this party started
