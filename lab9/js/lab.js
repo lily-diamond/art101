b3861a82cf11e4c7aca644105867c92813822912
lily-diamond <brielle.laplante22@gmail.com>
2025-05-12 14:08:51 -0700

    Lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,22 +1,45 @@
-var index = 0;
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
 
-let aColors = ["purple", "blue", "pink", "red"];
+   Requirements: jQuery must be loaded for this script to work.
 
-function fDoColors(color)
-{
+   Author: Brielle LaPlante
+   Date: 5/12/2025
+*/
+
+//add button to all sections
+$(".special-section").append("<button class = 'special-button-button'>Make Special</button>");
+
+//add the click listener
+$(".special-button-button").click(function(){
+    $(this).parent().toggleClass("special");
+});
+
+
+
+
+//
+// work we did in section:
+//
+// var index = 0;
+
+// let aColors = ["purple", "blue", "pink", "red"];
+
+// function fDoColors(color)
+// {
     
-    $("#output").css("background", color);
-    console.log(color);
+//     $("#output").css("background", color);
+//     console.log(color);
 
-    let newColor = color + " " + aColors[index];
-    console.log(newColor);
+//     let newColor = color + " " + aColors[index];
+//     console.log(newColor);
 
-    index++;
-}
+//     index++;
+// }
 
 //fDoColors("purple");
 
 //let doColors = fDoColors;
-aColors.map(fDoColors);
+//aColors.map(fDoColors);
 
 //setTimeout(fDoColors,2000);
