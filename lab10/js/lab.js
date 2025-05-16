7f1641c422efd3a14a6848e84f757013920b0592
lily-diamond <brielle.laplante22@gmail.com>
2025-05-15 19:22:13 -0700

    Lab 10 added

---
+++ lab10/js/lab.js
@@ -0,0 +1,24 @@
+// index.js - text conversation
+// Author: Brielle LaPlante
+// Date: 5/15/25
+
+// Constants
+
+// Functions
+
+//function given in lab instructions
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+
+$(".make-convo").click(function(){
+  const text = generateRandomText();
+  $("#output").append("<div class=\"text\">"+text+"</div>");
+});
